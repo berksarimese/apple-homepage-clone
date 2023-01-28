ea1ca87a2fc0a75d8258cfa6ca526858ab6d1583
berksarimese <berksarimese@hotmail.com>
2023-01-28 18:47:27 +0300

    Footer - Footer Responsive Update

--- index.js
+++ index.js
@@ -6,7 +6,34 @@ $(document).ready(function () {
             burger.show;
             burger.css('height','100vh');
         });
+    });
 
-    
+    let foTitle = $('.footer-container-mobile').children('.footer-column').children('.footer-title');
+    $(foTitle).click(function(){
+        let foList = $(this).parent('.footer-column').children('.footer-list');
+        let foPlus = $(this).children('.list-plus');
+        if(foList.css('display') == 'none') { 
+            $(foPlus).animate({ deg: 45 }, { duration: 200, step: function(now) {
+                    $(this).css({ transform: 'rotate(' + now + 'deg)' });
+                }});
+            //foPlus.css('transform', 'rotate(45deg)');      
+            foList.toggle('fast',function(){
+                this.show;
+            });
+        } else {
+            $(foPlus).animate(
+                { deg: 0 },
+                {
+                duration: 200,
+                step: function(now) {
+                    $(this).css({ transform: 'rotate(' + now + 'deg)' });
+                }
+                }
+            );
+            //foPlus.css('transform', 'rotate(0deg)'); 
+            foList.toggle('fast',function(){
+                this.show;
+            });
+        }
     });
 });
